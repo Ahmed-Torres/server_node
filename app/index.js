972994db4ff4dd4dbc295875b8c48f75cf0bba78
Yamil <torresyamil091@gmail.com>
2020-09-24 20:16:44 -0300

    nodejs moment nodemon instalado global

--- app/index.js
+++ app/index.js
@@ -7,3 +7,7 @@ const hobbies = ["correr", "nadar", "caminar"]
 let algoParaImprimir = "Algo que imprimimos desde la App"
 recorre.recorrerH(hobbies)
 imprime.imprimirAlgo(algoParaImprimir)
+
+let moment = require("moment")
+
+console.log("hola mundo " + moment(new Date()).format("DD/MM/yyyy"))
